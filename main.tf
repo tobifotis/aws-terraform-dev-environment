a501bb7c9c44a6c1b085cb840a23a56ee06b8dcf
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2026-01-08 15:49:38 -0600

    Create base VPC with DNS support

---
+++ main.tf
@@ -0,0 +1,74 @@
+# VPC
+resource "aws_vpc" "tobi_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "dev"
+  }
+}
+
+# SUBNET
+resource "aws_subnet" "tobi_subnet" {
+  vpc_id                  = aws_vpc.tobi_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+# INTERNET GATEWAY
+resource "aws_internet_gateway" "tobi_gw" {
+  vpc_id = aws_vpc.tobi_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+# ROUTE TABLE
+resource "aws_route_table" "tobi_public_rt" {
+  vpc_id = aws_vpc.tobi_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+# ROUTE
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.tobi_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.tobi_gw.id
+}
+
+# ROUTE TABLE ASSOCIATION
+resource "aws_route_table_association" "tobi_public_assoc" {
+  subnet_id      = aws_subnet.tobi_subnet.id
+  route_table_id = aws_route_table.tobi_public_rt.id
+}
+
+# SECURITY GROUP
+resource "aws_security_group" "tobi_sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.tobi_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Not best practice
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
