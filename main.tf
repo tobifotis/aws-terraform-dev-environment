1af2ff2f3e3c2d9c70603966536d9304544118b8
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2026-01-08 22:43:39 -0600

    Add EC2 dev instance using Ubuntu AMI data source

--- main.tf
+++ main.tf
@@ -72,3 +72,26 @@ resource "aws_security_group" "tobi_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# KEY PAIR
+resource "aws_key_pair" "tobi_auth" {
+  key_name   = "vscodekey"
+  public_key = file("~/.ssh/vscodekey.pub")
+}
+
+# EC2 INSTANCE
+resource "aws_instance" "dev_node" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.tobi_auth.id
+  vpc_security_group_ids = [aws_security_group.tobi_sg.id]
+  subnet_id              = aws_subnet.tobi_subnet.id
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev-node"
+  }
+}
