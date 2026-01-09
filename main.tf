f973ee6b7462907b9fd68b633c30a3dff85564b1
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2026-01-08 23:25:08 -0600

    Add user data script to bootstrap EC2 with Docker

--- main.tf
+++ main.tf
@@ -86,6 +86,7 @@ resource "aws_instance" "dev_node" {
   key_name               = aws_key_pair.tobi_auth.id
   vpc_security_group_ids = [aws_security_group.tobi_sg.id]
   subnet_id              = aws_subnet.tobi_subnet.id
+  user_data = file("userdata.tpl")
 
   root_block_device {
     volume_size = 10
